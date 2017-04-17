25c0e7b2e33cc9c492845c2d57ed35f36e833809
scm197 <scm197@rutgers.edu>
2017-04-16 23:10:14 -0400

    makes changes to downloaad images in batches

--- google-images-download.py
+++ google-images-download.py
@@ -103,57 +103,59 @@ while i<len(search_keyword):
     print ("\n")
     i = i+1
 
+    ## To save imges to the same directory
+    # IN this saving process we are just skipping the URL if there is any error
+
+    print ("Starting Download...")
+    k=0
+    errorCount=0
+    while(k<len(items)):
+        from urllib2 import Request,urlopen
+        from urllib2 import URLError, HTTPError
+
+        try:
+            req = Request(items[k], headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
+            response = urlopen(req)
+            output_file = open(str(k+1)+".jpg",'wb')
+            data = response.read()
+            output_file.write(data)
+            response.close();
+
+            print("completed ====> "+str(k+1))
+
+            k=k+1;
+
+        except IOError:   #If there is any IOError
+
+            errorCount+=1
+            print("IOError on image "+str(k+1))
+            k=k+1;
+
+        except HTTPError as e:  #If there is any HTTPError
+
+            errorCount+=1
+            print("HTTPError"+str(k))
+            k=k+1;
+        except URLError as e:
+
+            errorCount+=1
+            print("URLError "+str(k))
+            k=k+1;
+
+    print("\n")
+    print("All are downloaded")
+    print("\n"+str(errorCount)+" ----> total Errors")
+
 
     #This allows you to write all the links into a test file. This text file will be created in the same directory as your code. You can comment out the below 3 lines to stop writing the output to the text file.
     info = open('output.txt', 'a')        #Open the text file called database.txt
     info.write(str(i) + ': ' + str(search_keyword[i-1]) + ": " + str(items) + "\n\n\n")         #Write the title of the page
     info.close()                            #Close the file
 
+    items = []
+
+
 t1 = time.time()    #stop the timer
 total_time = t1-t0   #Calculating the total time required to crawl, find and download all the links of 60,000 images
 print("Total time taken: "+str(total_time)+" Seconds")
-print ("Starting Download...")
-
-## To save imges to the same directory
-# IN this saving process we are just skipping the URL if there is any error
-
-k=0
-errorCount=0
-while(k<len(items)):
-    from urllib2 import Request,urlopen
-    from urllib2 import URLError, HTTPError
-
-    try:
-        req = Request(items[k], headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
-        response = urlopen(req)
-        output_file = open(str(k+1)+".jpg",'wb')
-        data = response.read()
-        output_file.write(data)
-        response.close();
-
-        print("completed ====> "+str(k+1))
-
-        k=k+1;
-
-    except IOError:   #If there is any IOError
-
-        errorCount+=1
-        print("IOError on image "+str(k+1))
-        k=k+1;
-
-    except HTTPError as e:  #If there is any HTTPError
-
-        errorCount+=1
-        print("HTTPError"+str(k))
-        k=k+1;
-    except URLError as e:
-
-        errorCount+=1
-        print("URLError "+str(k))
-        k=k+1;
-
-print("\n")
-print("All are downloaded")
-print("\n"+str(errorCount)+" ----> total Errors")
-
 #----End of the main program ----#
